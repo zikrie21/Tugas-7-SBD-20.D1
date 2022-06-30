b05cdca429dc69329f39834dec367b07c989ea68
zikri hadiansyah <zikri.hadiansyah21@gmail.com>
2022-06-30 07:23:11 +0700

    menambahkan file nav

--- nav.php
+++ nav.php
@@ -4,14 +4,8 @@
       </li>
     </ul>
 
-    <!-- SEARCH FORM -->
-
-
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-      <!-- Messages Dropdown Menu -->
-      
-      <!-- Notifications Dropdown Menu -->
       
       <li class="nav-item">
         <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
